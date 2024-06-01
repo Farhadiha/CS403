915d8f313dfee8a845b30cd31518134753f4affc
Farhad <farhadiha@code.edu.az>
2024-06-02 01:55:36 +0400

    feat: homework

--- homework/taskHTML/script.js
+++ homework/taskHTML/script.js
@@ -1,21 +1,24 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const username = document.getElementById('username').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirmPassword').value
+    const gender = document.querySelector('input[name="gender"]:checked').value
 
     if (password !== confirmPassword) {
-        alert('Confirm password did not match with password');
-        return;
+        alert('Confirm password did not match with password')
+        return
     }
 
-    console.log('Username:', username);
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('Confirm Password:', confirmPassword);
-    console.log('Gender:', gender);
+    const userDetails = {
+        username: username,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        gender: gender
+    }
 
+    console.log(userDetails);
 });
