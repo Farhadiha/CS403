ba389249e8f5a72cef871fe17b90501eb0178345
Farhad <farhadiha@code.edu.az>
2024-06-02 12:24:07 +0400

    feat: homework

---
+++ homework/taskHTML/form.js
@@ -0,0 +1,34 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.getElementById('loginForm');
+//     const submitBtn = document.getElementById('submitBtn');
+
+//     form.addEventListener('input', function() {
+//         const isFormValid = form.checkValidity();
+//         submitBtn.disabled = !isFormValid;
+//     });
+
+//     form.addEventListener('submit', function(event) {
+//         event.preventDefault();
+
+//         const username = document.getElementById('username').value;
+//         const email = document.getElementById('email').value;
+//         const password = document.getElementById('password').value;
+//         const confirmPassword = document.getElementById('confirmPassword').value;
+//         const gender = document.querySelector('input[name="gender"]:checked').value;
+
+//         if (password !== confirmPassword) {
+//             alert('Confirm password did not match with password');
+//             return;
+//         }
+
+//         const userDetails = {
+//             username: username,
+//             email: email,
+//             password: password,
+//             confirmPassword: confirmPassword,
+//             gender: gender
+//         };
+
+//         console.log(userDetails);
+//     });
+// });
--- homework/taskHTML/script.js
+++ homework/taskHTML/script.js
@@ -1,24 +1,37 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault()
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('loginForm')
+    const submitBtn = document.getElementById('submitBtn')
 
-    const username = document.getElementById('username').value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    const confirmPassword = document.getElementById('confirmPassword').value
-    const gender = document.querySelector('input[name="gender"]:checked').value
 
-    if (password !== confirmPassword) {
-        alert('Confirm password did not match with password')
-        return
-    }
+    
+    form.addEventListener('input', function() {
+        const isFormValid = form.checkValidity()
+        submitBtn.disabled = !isFormValid
+    })
 
-    const userDetails = {
-        username: username,
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword,
-        gender: gender
-    }
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
 
-    console.log(userDetails);
+        const username = document.getElementById('username').value
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        const confirmPassword = document.getElementById('confirmPassword').value
+        const gender = document.querySelector('input[name="gender"]:checked').value
+
+        if (password !== confirmPassword) {
+            alert('Confirm password did not match with password')
+            return
+        }
+
+        const userDetails = {
+            username: username,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword,
+            gender: gender
+        };
+
+        console.log(userDetails);
+    });
 });
+
